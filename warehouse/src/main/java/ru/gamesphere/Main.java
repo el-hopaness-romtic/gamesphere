3f16a31621b592a41466da4f046ffdb04fb058c4
el-hopaness-romtic <60661917+el-hopaness-romtic@users.noreply.github.com>
2022-11-13 19:56:32 +0300

    дз5 скрипт создания базы

--- warehouse/src/main/java/ru/gamesphere/Main.java
+++ warehouse/src/main/java/ru/gamesphere/Main.java
@@ -1,7 +1,22 @@
 package ru.gamesphere;
 
+import org.flywaydb.core.Flyway;
+
+import java.util.Locale;
+
 public class Main {
+    static {
+        Locale.setDefault(new Locale("en", "GB"));
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArgsParser parser = new ArgsParser(args);
+        Flyway.configure()
+                .dataSource(parser.getJdbcUrl(),
+                        parser.getUsername(),
+                        parser.getPassword())
+                .locations("db")
+                .load()
+                .migrate();
     }
 }
